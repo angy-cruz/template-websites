9bb8d4aa95bf9b39e5d9d0bcb7648991d525c5ed
Angy <angelica.cruz@oneparkfinancial.com>
2024-05-07 16:50:16 -0600

    More sections

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,4 +1,4 @@
-<!-- HERO 01 -->
+<!-- HERO 01 GRADIENT -->
 <section id="hero"
     class="text-center flex flex-col md:flex-row items-center justify-center h-[650px] md:h-[600px] mt-[90px] md:mt-0 bg-hero-mobile bg-bottom bg-cover bg-no-repeat md:bg-secondary ">
     <div
@@ -26,7 +26,7 @@
 </section>
 
 
-<!-- HERO 02 -->
+<!-- HERO 02 RECUADRO-->
 <section id="hero"
     class="text-center flex flex-col md:flex-row items-center justify-center h-[800px] md:h-[600px] mt-[60px] md:mt-0 md:top-[20px] bg-hero-mobile md:bg-hero-desktop bg-center bg-cover bg-no-repeat">
     <div class="flex flex-col justify-center items-center w-[80%] md:w-1/2 h-[70%] md:pt-24 md:h-full">
@@ -48,3 +48,33 @@
     </div>
 </section>
 
+
+<!-- HERO WITH LOGO -->
+<section id="hero"
+    class="text-center flex flex-col md:flex-row items-center justify-center h-[650px] md:h-[600px] mt-[90px] md:mt-0 bg-hero-mobile bg-bottom bg-cover bg-no-repeat md:bg-secondary">
+    <div
+        class="hidden md:flex h-1/2 w-full md:h-full md:w-1/2 justify-center items-center pt-0 md:pt-[90px] max-h-[470px] md:max-h-full">
+        <img src="./img/banner-desktop.png" alt="Playground" class="object-cover h-full w-full" />
+    </div>
+    <div class="flex justify-center items-center w-full h-full md:w-1/2 md:pt-[100px] md:bg-secondary">
+        <div
+            class="bg-white p-6 md:bg-white/0 flex flex-col justify-center items-center w-[70%] md:h-full gap-8 text-mediumGray py-10 md:p-0">
+            <div class="w-full flex flex-col items-center gap-2">
+                <div class="h-20 md:h-32">
+                    <img src="img/logos/logo-full.png" alt="logo" class="object-contain h-full w-full">
+                </div>
+                <h1 class="w-full text-[28px] md:text-[40px] leading-12 md:text-center font-extrabold">
+                    We'll give you a fresh coat!
+                </h1>
+            </div>
+            <div class="w-full flex flex-col items-center">
+                <div class="flex flex-row items-center justify-center w-[90%] max-w-[342px]">
+                    <button type="submit"
+                        class="bg-primary text-white py-2 px-4 font-semibold text-lg flex gap-2 items-center justify-center w-full transition-all ease-in hover:bg-primary/50 focus:ring-4 focus:bg-primary focus:ring-pribg-primary/50 active:bg-opacity-0 active:ring-4 active:ring-pribg-primary/50 disabled:bg-opacity-50">
+                        Contact us
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
