b9c759b7992409124ebef5d87b67223eeee39494
Angy <152193815+angy-cruz@users.noreply.github.com>
2024-07-03 09:51:36 -0600

    Assets and styles for A&J Flooring and more

---
+++ config/captcha.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'service' => 'Recaptcha', // options: Recaptcha / Hcaptcha / Turnstile
+    'sitekey' => env('CAPTCHA_SITEKEY', ''),
+    'secret' => env('CAPTCHA_SECRET', ''),
+    'collections' => [],
+    'forms' => 'all',
+    'user_login' => false,
+    'user_registration' => false,
+    'disclaimer' => '',
+    'invisible' => false,
+    'hide_badge' => false,
+    'enable_api_routes' => false,
+    'custom_should_verify' => null,
+];
