ea4206720c1b2e2f38fb7a3a98f796a5b1e262ce
Angy <152193815+angy-cruz@users.noreply.github.com>
2024-07-24 16:49:58 -0600

    Update _footer.blade.php

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -36,6 +36,44 @@
 </footer>
 
 
+<!-- Footer english white -->
+
+<footer>
+    <section class="bg-white flex flex-col justify-center items-center py-16">
+        <div class="w-[90%] max-w-[1000px]">
+            <div class="w-full flex flex-col md:flex-row md:items-center justify-evenly pb-16">
+                <div class="flex flex-col items-center gap-6 md:max-w-[255px]">
+                    <div class="justify-self-center self-center flex justify-center items-center h-20">
+                        <img src="img/logos/logo-full-wt.png" alt="Ícono del logo" class="object-contain h-full w-full" />
+                    </div>
+                    <div class="text-black opacity-50 text-5xl flex flex-row justify-evenly mb-6 md:mb-0 gap-8">
+                        <a href="" target="_blank"><img src="./img/icons/facebook-w.png" alt="Icon facebook" /></a>
+                        <a href=""><img src="./img/icons/whatsapp-w.png" alt="Icon WhatsApp" /></a>
+                        <a href="" target="_blank"><img src="./img/icons/instagram-w.png" alt="Icon Instagram" /></a>
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center gap-y-4 mb-4 md:mb-0 text-black">
+                    <h3 class="font-extrabold">Sitemap</h3>
+                    <ul class="grid gap-y-4 underline opacity-60">
+                        <li class=""><a href="#hero">Home</a></li>
+                        <li><a href="#about">About us</a></li>
+                        <li><a href="#departments">Our services</a></li>
+                    </ul>
+                </div>
+                <div class="text-black opacity-60 flex flex-col justify-center">
+                    <ul class="grid gap-y-4 underline">
+                        <li><a href="#reviews">Reviews</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#location">Location</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="opacity-50 w-[90%]" />
+        <span class="sm:text-center text-black opacity-60 pt-8 mb-8 w-[90%] md:w-3/5">{{ now()->year }}, M.J. Autoservice. All rights reserved. Developed by One Park Financial.</span>
+    </section>
+</footer>
+
 <!-- FOOTER SPANISH -->
 <footer>
     <section class="bg-darkGray flex flex-col justify-center items-center py-16">
