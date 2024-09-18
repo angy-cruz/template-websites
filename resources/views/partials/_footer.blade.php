35aac4b225723d85c8b96835b519238e22fde0da
Angy <angy@krodox.com>
2024-09-18 13:28:48 -0600

    Update _footer.blade.php

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -112,3 +112,44 @@
             Stone. All rights reserved. Developed by One Park Financial.</span>
     </section>
 </footer>
+
+
+<!-- Footer spanish white -->
+<footer>
+    <section class="bg-white flex flex-col justify-center items-center py-16">
+        <div class="w-[90%] max-w-[1000px]">
+            <div class="w-full flex flex-col md:flex-row md:items-center justify-evenly pb-16">
+                <div class="flex flex-col items-center gap-6 md:max-w-[255px]">
+                    <div class="justify-self-center self-center flex justify-center items-center h-20">
+                        <img src="img/logos/logo-full-wt.png" alt="Ícono del logo" class="object-contain h-full w-full" />
+                    </div>
+                    <div class=" opacity-50 text-5xl flex flex-row justify-evenly mb-6 md:mb-0 gap-8">
+                        <a href=""><img src="./img/icons/facebook-w.png" alt="Icon facebook" /></a>
+                        <a href=""><img src="./img/icons/whatsapp-w.png" alt="Icon WhatsApp" /></a>
+                        <a href=""><img src="./img/icons/instagram-w.png" alt="Icon Instagram" /></a>
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center gap-y-4 mb-4 md:mb-0 ">
+                    <h3 class="font-extrabold">Mapa de sitio</h3>
+                    <ul class="grid gap-y-4 underline opacity-60">
+                        <li class=""><a href="#hero">Inicio</a></li>
+                        <li><a href="#products">Nuestro trabajo</a></li>
+                        <li><a href="#about">Acerca de nosotros</a></li>
+                    </ul>
+                </div>
+                <div class=" opacity-60 flex flex-col justify-center">
+                    <ul class="grid gap-y-4 underline">
+                        <br class="hidden md:inline-block" />                        
+                        <li><a href="#contact">Contacto</a></li>
+                        <li><a href="#location">Ubicación</a></li>
+                        <li><a href="#faqs">Preguntas frecuentes</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="opacity-60 w-[90%]" />
+        <span class="sm:text-center opacity-60 pt-8 mb-8 w-[90%] md:w-3/5">{{ now()->year }}, America Quality
+            Stone. All rights reserved. Developed by One Park Financial.</span>
+    </section>
+
+</footer>
